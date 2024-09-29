26bf6b8ea4373da5c68f2f555d069b65b5c12a1f
simplycleverlol <akgeorg2101@yandex.ru>
2024-09-30 02:39:43 +0300

    Доработки по части стабильности

--- pipeline_wordcloud.py
+++ pipeline_wordcloud.py
@@ -6,16 +6,22 @@ import asyncio
 MAX_BATCH = 300
 MAX_OUT = 60
 
-def strlistfmt(strs: Iterable):
-            for str in strs:
-                str = ''.join(s for s in str.strip() if s.isalpha() or s == ' ')
-                if len(str) and not ':' in str: yield str
+def response_formatter(response: str):
+    strs = response.split('\n')
+    ans = []
+    longcounter = 0
+    for str in strs:
+        str = ''.join(s for s in str.strip() if s.isalpha() or s == ' ')
+        nwords = len(str.split(' '))
+        if 0 < nwords <= 10 and not ':' in str: ans.append(str)
+        else: longcounter += 1
 
-def response_formatter(response):
-    response = response.split('\n')
-    return list(strlistfmt(response))
+        if longcounter >= 3:
+                print('HIT LONGCOUNTER')
+                return []
+    return ans 
 
-model = Synonyms('https://hack.agicotech.ru/api')
+model = Synonyms('https://hack.agicotech.ru/api', MAX_OUT)
 asyncio.run(model.check_model())
 
 async def tree_iteration(data : list):
@@ -23,17 +29,21 @@ async def tree_iteration(data : list):
     batch_size = (len(data) + batch_amount - 1) // batch_amount
     data_array = []
     for i in range(0, len(data), batch_size):
+        print(f'PROCESSED BATCH {i}')
         batch = data[i:i+batch_size]
         batch_res = await pipeline_array_words(batch)
         data_array += batch_res
     return data_array
 
 async def pipeline_array_words(data: Iterable[str] | str):
-    data = await model.process(data)
-    if data is None:
-        data = ''
-    data = response_formatter(data)
-    return data
+    input = '\n'.join(data)
+    for i in range(10):
+        data = await model.process(input)
+        data = response_formatter(data)
+        if len(data):
+            print(f'OK RESPONSE IN {i} REQUEST')
+            return data
+    return []
 
 def remove_multiple_spaces(s: str):
     s = ' '.join(subs for subs in s.split(' ') if len(subs))
@@ -46,6 +56,9 @@ async def pipeline_text(data: str | Iterable[str]):
     if type(data) == str: data = data.split('\n')
     data = list(remove_empty_lines_and_spaces(data))
     data = await tree_iteration(data)
-    while len(data) > MAX_OUT:
+    l = len(data)
+    while l > MAX_OUT:
+        print(f'HIT MAX_OUT : {l}/{MAX_OUT}')
         data = await tree_iteration(data)
+        l = len(data)
     return {s : i for i, s in enumerate(data[::-1], 1)}
--- synonyms.py
+++ synonyms.py
@@ -1,14 +1,14 @@
 from llama import llama
 from typing import Iterable
 class Synonyms:
-    def __init__(self, api_url):
+    def __init__(self, api_url, max_out):
         self.llama = llama(api_url, 'synonyms')
-    SYSTEM = """Ты - продвинутая языковая модель и твоя задача - анализ результатов опросов. Тебе нужно сгруппировать полученные в виде столбца строк ответы по смыслу в группы с синонимичным значением. В ответ давай ТОЛЬКО названия групп, каждое - от 1 до 3 слов в отельной строке"""
-    #SYSTEM = """Ты - продвинутая языковая модель и твоя задача - анализ результатов опросов. Среди всех предоставленных построчно записанных ответов объедини все синонимичные. Все ответы надо сократить до 1-3 слов с сохранением смысла и языка. В ответ дай ТОЛЬКО обработанную информацию - ответы, разделенные новой строкой, БЕЗ нумерации и прочих пояснений."""
+    #SYSTEM = """Ты - продвинутая языковая модель и твоя задача - анализ результатов опросов. Тебе нужно сгруппировать полученные в виде столбца строк ответы по смыслу в группы с синонимичным значением. В ответ давай ТОЛЬКО названия групп, каждое - от 1 до 3 слов в отельной строке"""
+        self.SYSTEM = f"""Ты - продвинутая языковая модель и твоя задача - анализ результатов опросов. Среди всех предоставленных построчно ответов объедини все синонимичные. Все ответы надо сократить до 1-3 слов с сохранением смысла и языка. В ответ дай ТОЛЬКО обработанную информацию - не более {max_out} самых популярных ответов, разделенные новой строкой, БЕЗ нумерации и прочих пояснений."""
 
-    MODELFILE = f"""FROM llama3.2:3b
+        self.MODELFILE = f"""FROM llama3.2:3b
 PARAMETER temperature 0.5
-SYSTEM {SYSTEM}
+SYSTEM {self.SYSTEM}
 """
     async def check_model(self):
         res, models = await self.llama.ps()
@@ -21,19 +21,12 @@ SYSTEM {SYSTEM}
         await self.llama.create('synonyms', self.MODELFILE)
 
 
-    async def process(self, data: str | Iterable):
-        if type(data) != str:
-            data = '\n'.join(data)
-        for _ in range(10):
-            res, json = await self.llama.generate(prompt=data, system=self.SYSTEM)
-            if not res: continue
-            text = json.get('response', '')
-            if len(text) == 0: continue
-            text = [s for s in text.split('\n') if 0 < len(s.split(' ')) <= 10]
-            if len(text):
-                return '\n'.join(text)
-            
-        return  'Unsucceeded to process :('
+    async def process(self, data: str):
+        res, json = await self.llama.generate(prompt=data, system=self.SYSTEM)
+        if not res: return ''
+        text = json.get('response', '')
+        print(text)
+        return text
 
 async def main():
     syn = Synonyms('https://hack.agicotech.ru/api')
