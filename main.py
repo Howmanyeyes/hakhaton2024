4b438ffe9f6ab12e8dcb3f955773ae149adf778f
SevaDach <and.ppv@yandex.ru>
2024-09-30 17:31:38 +0300

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 try:
-    from fastapi import FastAPI, File, UploadFile, Form
-    from fastapi.responses import HTMLResponse, JSONResponse, FileResponse
-    from pathlib import Path
-    # import shutil
-    import threading
+    from fastapi import FastAPI, File, UploadFile
     import uvicorn
     import pandas as pd
-    import io
     import os
     import asyncio
     import json
@@ -14,7 +9,6 @@ try:
     import string
     import matplotlib.pyplot as plt
     from wordcloud import WordCloud
-    from fastapi.responses import StreamingResponse
     from pipeline_wordcloud import pipeline_text
     from fastapi.staticfiles import StaticFiles
 
@@ -35,8 +29,8 @@ def ind_exel(n):
     if n == 0:
         return ''
     while n > 0:
-        n -= 1  # Adjust for 0-indexed logic (Excel is 1-indexed)
-        result = chr(n % 26 + 65) + result  # Convert to A-Z (65 is ASCII 'A')
+        n -= 1  
+        result = chr(n % 26 + 65) + result  # 65 = 'A'
         n //= 26
     return result+': '
 
@@ -44,7 +38,9 @@ def get_upload(file):
     f_content = file.file._file
     f_type = file.filename.split(".")[-1]
     f_name = generate_name()
-    best_colormaps = ['MTC Special','viridis', 'plasma', 'inferno', 'magma', 'cividis', 'Spectral', 'coolwarm', 'YlGnBu', 'RdYlBu', 'PuBuGn', 'hsv']
+    best_colormaps = ['MTC Special','viridis', 'plasma', 'inferno', 'magma', \
+                      'cividis', 'Spectral', 'coolwarm', 'YlGnBu', 'RdYlBu', \
+                      'PuBuGn', 'hsv']
     if f_type == 'csv':
         df = pd.read_csv(f_content)
     elif f_type == 'xlsx':
@@ -54,19 +50,19 @@ def get_upload(file):
             f.write(f_content.getvalue())
         return {
             "inputs": {
-                "choose color scheme of clowd": {"type": "dropdown", "items": best_colormaps, "default": "red"},
+                "choose color scheme of clowd": {"type": "dropdown", \
+                                                 "items": best_colormaps, "default": "red"},
                 "filter profanity": {"type": "checkbox", "default": True}
             },
             "target_id": f"{f_name}.txt",
         }
     df.to_pickle(f"temp_tables/{f_name}.pkl")
     
-    cols = [f'{ind_exel(int(num))}{x}' for num, x in enumerate(["My info is in row"] + list(df.columns), 0)]
-    # rows = len(df) не нужно пока что 
-        
+    cols = [f'{ind_exel(int(num))}{x}' for num, x in enumerate(["My info is in row"] + list(df.columns), 0)]        
     return {
         "inputs": {
-            "choose color scheme of clowd": {"type": "dropdown", "items": best_colormaps, "default": "red"},
+            "choose color scheme of clowd": {"type": "dropdown", \
+                                             "items": best_colormaps, "default": "red"},
             "choose column": {"type": "dropdown", "items": cols},
             "type number of row (ONLY if you use rows)": {"type": "text", "default": ""},
             "filter profanity": {"type": "checkbox", "default": True}
@@ -79,6 +75,7 @@ async def a_get_upload(file):
 
 @app.post('/rest/upload')
 async def return_upload(file: UploadFile = File(...)):
+    """Returns a dictionary with the uploaded file's name and possible inputs."""
     return await a_get_upload(file)
 
 with open('bad_words.json', 'r') as f:
@@ -88,27 +85,26 @@ def filter_profanity(answers):
     l_words = [x for x in answers if type(x) == str] # and len(x) < 3]
     words = [x for x in l_words if x.lower() not in BAD_WORDS]
     return words
+
 def red_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
-        red_value = 255  # Maximum red value to keep it bright
-        green_value = random.randint(0, 50)  # Small random value for green (to keep red dominant)
-        blue_value = random.randint(0, 50)  # Small random value for blue (to keep red bright)
+        red_value = 255  
+        green_value = random.randint(0, 50)
+        blue_value = random.randint(0, 50)
         return f"rgb({red_value}, {green_value}, {blue_value})"
 
 def create_wordcloud(rating, colour, name):
-    # Create a WordCloud object with the specified color
     MTC = False
     if colour == "MTC Special":
         colour = 'viridis'
         MTC = True
-    wordcloud = WordCloud(width=1000, height=1000, background_color="white", colormap=colour, random_state=42, font_path='MTSWide-Bold.ttf').generate_from_frequencies(rating)
+    wordcloud = WordCloud(width=1000, height=1000, background_color="white", \
+                          colormap=colour, random_state=42, font_path='MTSWide-Bold.ttf')\
+                            .generate_from_frequencies(rating)
     if MTC:
         wordcloud.recolor(color_func=red_color_func)
-    # Display the generated word cloud
     plt.figure(figsize=(10, 10))
     plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")  # No axis for cleaner look
-
-    # Save the word cloud as a PNG file with the given name
+    plt.axis("off")  
     file_name = f"{name}.png"
     plt.savefig(f"generated/{file_name}", format="png", bbox_inches="tight")
     plt.close()
@@ -126,7 +122,7 @@ def get_answers(request: dict):
         if inputs['type number of row (ONLY if you use rows)'] != '':
             answers = df.iloc[int(inputs['type number of row (ONLY if you use rows)'])]
         else:
-            return 0 # return error - polzovatel dolbaeb
+            return 0 # return error - polzovatel ne vvodil chislo
     else:
         answers = df[': '.join(inputs['choose column'].split(': ')[1:])]
 
@@ -143,20 +139,16 @@ async def a_get_answers(request: dict):
 
 @app.post('/rest/process/')
 async def return_image(request: dict):
+    """Returns image url of wordcloud in form of dict"""
     answers = await a_get_answers(request=request)
-    # ретюрнить ансеры в пайплайн
-    rating = await pipeline_text(answers) # заглушка, с тобой делать надо, дебагер выдает ошибку:     while len(data) > 60: TypeError: object of type 'coroutine' has no len()
-    
-
+    rating = await pipeline_text(answers) 
     colour = request['inputs']['choose color scheme of clowd']
     create_wordcloud(rating, colour, request['target_id'])
     return {
         "image_url": f"/generated/{request['target_id']}.png"
     }
     
-
 app.mount("/", StaticFiles(directory="frontend",html = True), name="static")
-
 
 if __name__ =='__main__':
     import uvicorn
